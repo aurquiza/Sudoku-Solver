fb611161c92c391e6170664d7872044a26536692
Alexis <aurqui7@uic.edu>
2017-10-14 23:40:07 -0500

    new branch and new print statement

--- src/GuiLayout.java
+++ src/GuiLayout.java
@@ -4,6 +4,7 @@ public class GuiLayout {
 	public static void main(String[] args) 
 	{
 		System.out.println("testing by adding a String");
+		System.out.println("slowly getting used to git");
 	}
 
 }
