6f8f9531c9d40f602d75c59fb67294c5fe340f79
Alexis <aurqui7@uic.edu>
2017-10-15 00:01:28 -0500

    adding another print statement...

--- src/GuiLayout.java
+++ src/GuiLayout.java
@@ -5,6 +5,7 @@ public class GuiLayout {
 	{
 		System.out.println("testing by adding a String");
 		System.out.println("slowly getting used to git");
+		System.out.println("trying out if my key worked");
 	}
 
 }
