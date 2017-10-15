b6bdeec32184fb32cb32fb3f67ff7d4412a9d4ca
eleon23 <32808558+eleon23@users.noreply.github.com>
2017-10-15 00:31:34 -0500

    Update GuiLayout.java

--- src/GuiLayout.java
+++ src/GuiLayout.java
@@ -6,6 +6,7 @@ public class GuiLayout {
 		System.out.println("testing by adding a String");
 		System.out.println("slowly getting used to git");
 		System.out.println("trying out if my key worked");
+		System.out.println("I cri err time, plz halp troy sprrreaddin cheeks too far");
 	}
 
 }
