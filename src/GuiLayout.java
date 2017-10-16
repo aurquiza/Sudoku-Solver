dde89650f59bdad2d10f27c7e5189f809cc18774
Jakub <jglebo2@uic.edu>
2017-10-16 17:40:41 -0500

    added a print statement

--- src/GuiLayout.java
+++ src/GuiLayout.java
@@ -7,7 +7,7 @@ public class GuiLayout {
 	public static void main(String[] args) 
 	{
 		System.out.println("Eric Leon(eleon23), Alexis Urquiz(aurqui7)");
-		
+		System.out.println("hi");
 		System.out.println("Jicub(jglebo2)");
 		int x = 0;
 		
