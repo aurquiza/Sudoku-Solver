ebe683c7b02d74955dc0ea537a6bbc46ffce6e9a
Eric Leon <eleon23@uic.edu>
2017-10-16 17:56:20 -0500

    I deleted one Print statement

--- src/GuiLayout.java
+++ src/GuiLayout.java
@@ -8,7 +8,7 @@ public class GuiLayout {
 	{
 		System.out.println("Eric Leon(eleon23), Alexis Urquiz(aurqui7)");
 		System.out.println("hi");
-		System.out.println("Jicub(jglebo2)");
+		
 		int x = 0;
 		
 	}
