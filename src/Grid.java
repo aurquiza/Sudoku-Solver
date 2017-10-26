34ed6ded04e362f5c91f863d4b67cf0501871fc4
Alexis <aurqui7@uic.edu>
2017-10-26 11:03:09 -0500

    added gui

--- src/Grid.java
+++ src/Grid.java
@@ -91,100 +91,107 @@ public class Grid extends GridLayout
 		return gridSections;
 	}
 
+	// getter method that acts like a 2d array to access buttons within the
+	// subgrid class
+	public buttonClass[] getSubAtCellAt(int subgrid)
+	{
+
+		return gridSections[subgrid].getCells();
+	}
 
 /*
-   Name: Alexis Urquiza, Eric Leon, Jakub Glebocki
-   NetID: aurqui7, eleon23, jglebo2
+	Name: Alexis Urquiza, Eric Leon, Jakub Glebocki
+	NetID: aurqui7, eleon23, jglebo2
 
-   - This class consists of code that creates a JPanel that will hold a specified number of cells
+	- This class consists of code that creates a JPanel that will hold a specified number of cells
 
-   - The extension of the JPanel is used to add features such as an array of buttons that each will
-     hold information about a specific cell as well as an integer that specifies a sub-grid's number
+	- The extension of the JPanel is used to add features such as an array of buttons that each will
+	  hold information about a specific cell as well as an integer that specifies a sub-grid's number
 */
-private class SubGrid extends JPanel
-{
-	// instance variables
-	private int SubGridSection;
-	private buttonClass cells[];
-
-	// constructor
-	// @Param grid - Layout manager that will be used for the JPanel
-	// @Param flag - distinction between creating subgrid for the sudoku grid or input button grid
-	// @Param section - number that will be given to the specified subgrid
-	// @Param numOfCells - number of cells that should be in the sub-grid
-	public SubGrid(GridLayout grid, boolean flag, int section, int numOfCells)
-	{	
-		// call super constructor
-		super(grid);
-
-		//assign grid number
-		SubGridSection = section;
-		cells = new buttonClass[numOfCells];
-
-		// true, meaning create the input buttons for cells of the sudoku grid
-		// false, meaning create sub-grid for the sudoku grid
-		if(flag)
-			createInputButtons();
-		else
-			createCells();
-		
-
-	}
-
-	// create and add cells to the JPanel
-	private void createCells()
+	private class SubGrid extends JPanel
 	{
-		//initialize each cell and add to the JPanel
-		for(int i = 0; i < cells.length; i++)
-		{
-			cells[i] = new buttonClass(" ", SubGridSection);
-			cells[i].addActionListener(
-				new ActionListener()
-				{
-					public void actionPerformed(ActionEvent event)
-					{
-						buttonClass b = (buttonClass) event.getSource();
-						b.setCellValue(currentInput);
-					}
-				}
-			);
-			add(cells[i]);
-		}
-	}
+		// instance variables
+		private int SubGridSection;
+		private buttonClass cells[];
 
-	// create buttons for the sidebar of the gui
-	private void createInputButtons()
-	{	
-		// loops through 9 of the 11 buttons created
-		for(int i = 0; i < cells.length - 2; i++)
-		{
-			cells[i] = new buttonClass(Integer.toString(i + 1), i + 1);
-			cells[i].addActionListener(
-				new ActionListener()
-				{
-					public void actionPerformed(ActionEvent event)
-					{
-						buttonClass b = (buttonClass) event.getSource();
-						setInput(Integer.toString(b.getCellSection()));
-					}
-				}
-			);
-			add(cells[i]);
+		// constructor
+		// @Param grid - Layout manager that will be used for the JPanel
+		// @Param flag - distinction between creating subgrid for the sudoku grid or input button grid
+		// @Param section - number that will be given to the specified subgrid
+		// @Param numOfCells - number of cells that should be in the sub-grid
+		public SubGrid(GridLayout grid, boolean flag, int section, int numOfCells)
+		{	
+			// call super constructor
+			super(grid);
+
+			//assign grid number
+			SubGridSection = section;
+			cells = new buttonClass[numOfCells];
+
+			// true, meaning create the input buttons for cells of the sudoku grid
+			// false, meaning create sub-grid for the sudoku grid
+			if(flag)
+				createInputButtons();
+			else
+				createCells();
+			
+
 		}
 
-		// special initialization for the "X" button
-		cells[cells.length - 2] = new buttonClass("X", 10);
-		add(cells[cells.length - 2]);
+		// create and add cells to the JPanel
+		private void createCells()
+		{
+			//initialize each cell and add to the JPanel
+			for(int i = 0; i < cells.length; i++)
+			{
+				cells[i] = new buttonClass(" ", SubGridSection);
+				cells[i].addActionListener(
+					new ActionListener()
+					{
+						public void actionPerformed(ActionEvent event)
+						{
+							buttonClass b = (buttonClass) event.getSource();
+							b.setCellValue(currentInput);
+						}
+					}
+				);
+				add(cells[i]);
+			}
+		}
 
-		// special initialization for the "?" button
-		cells[cells.length - 1] = new buttonClass("?", 11);
-		add(cells[cells.length - 1]);
-	}
+		// create buttons for the sidebar of the gui
+		private void createInputButtons()
+		{	
+			// loops through 9 of the 11 buttons created
+			for(int i = 0; i < cells.length - 2; i++)
+			{
+				cells[i] = new buttonClass(Integer.toString(i + 1), i + 1);
+				cells[i].addActionListener(
+					new ActionListener()
+					{
+						public void actionPerformed(ActionEvent event)
+						{
+							buttonClass b = (buttonClass) event.getSource();
+							setInput(Integer.toString(b.getCellSection()));
+						}
+					}
+				);
+				add(cells[i]);
+			}
 
-	// getter method that returns array of cells
-	public buttonClass[] getCells()
-	{
-		return cells;
+			// special initialization for the "X" button
+			cells[cells.length - 2] = new buttonClass("X", 10);
+			add(cells[cells.length - 2]);
+
+			// special initialization for the "?" button
+			cells[cells.length - 1] = new buttonClass("?", 11);
+			add(cells[cells.length - 1]);
+		}
+
+		// getter method that returns array of cells
+		public buttonClass[] getCells()
+		{
+			return cells;
+		}
 	}
-}
 }
